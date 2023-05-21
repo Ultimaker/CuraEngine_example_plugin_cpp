1ec75b06645d0a96134bc5a38bca27dafadef109
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-05-21 18:49:46 +0200

    Cleaned up code
    In the file include/plugin/plugin.h:
    
    The entire contents of the file have been deleted.
    In the file src/main.cpp:
    
    Various header includes have been added, including agrpc/asio_grpc.hpp, boost/asio/co_spawn.hpp, boost/asio/detached.hpp, boost/asio/signal_set.hpp, grpcpp/server.h, grpcpp/server_builder.h, range/v3/view/concat.hpp, range/v3/view/single.hpp, and spdlog/spdlog.h.
    The plugin namespace has been removed.
    The plugin_services struct has been removed.
    A new plugin_metadata struct has been added, which contains metadata related to the plugin.
    The main function has been modified to include the plugin metadata and implement the plugin functionality using gRPC.
    The plugin_runner object has been removed.
    The gRPC server and services are now created and registered within the main function.
    The plugin handshake and main process functionality have been implemented as separate coroutines within the main function.
    In the file conanfile.py:
    
    The package requirement for curaengine_grpc_definitions has been changed from @ultimaker/testing to @ultimaker/arcus_replacement.
    These are the main changes introduced by the patch you provided. Let me know if you have any specific questions or need further assistance.
    
    [CURA-10475]

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,34 @@
+#include <agrpc/asio_grpc.hpp>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/signal_set.hpp>
 #include <docopt/docopt.h> // Library for parsing command line arguments
 #include <fmt/format.h> // Formatting library
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <range/v3/view/concat.hpp>
+#include <range/v3/view/single.hpp>
 #include <spdlog/spdlog.h> // Logging library
 
 #include "plugin/cmdline.h" // Custom command line argument definitions
-#include "plugin/plugin.h"
+#include "simplify/simplify.h" // Custom utilities for simplifying code
+
+#include "simplify.grpc.pb.h"
+#include "simplify.pb.h"
 
 namespace asio = boost::asio;
 namespace proto = cura::plugins::proto;
-using plugin_runner_t = plugin::plugin_services<proto::Simplify::AsyncService, &proto::Simplify::AsyncService::RequestSimplify, proto::SimplifyResponse, proto::SimplifyRequest>;
 
+
+struct plugin_metadata
+{
+    cura::plugins::proto::SlotID slot_id{ cura::plugins::proto::SlotID::SIMPLIFY };
+    std::string plugin_name{ "UltiMaker basic simplification" };
+    std::string slot_version{ "0.1.0-alpha.1+grpc_tests" };
+    std::string plugin_version{ "0.2.0-alpha.1+proto_tests" };
+};
+
+static plugin_metadata metadata{};
 
 int main(int argc, const char** argv)
 {
@@ -16,6 +36,96 @@ int main(int argc, const char** argv)
     constexpr bool show_help = true;
     const std::map<std::string, docopt::value> args = docopt::docopt(fmt::format(plugin::cmdline::USAGE, plugin::cmdline::NAME), { argv + 1, argv + argc }, show_help, plugin::cmdline::VERSION_ID);
 
-    plugin_runner_t plugin_runner{ args.at("<address>").asString(), static_cast<uint32_t>(args.at("<port>").asLong()) };
-    plugin_runner();
+    grpc::ServerBuilder builder;
+    agrpc::GrpcContext grpc_context{ builder.AddCompletionQueue() };
+    builder.AddListeningPort(fmt::format("{}:{}", args.at("<address>").asString(), args.at("<port>").asString()), grpc::InsecureServerCredentials());
+
+    proto::Simplify::AsyncService service;
+    builder.RegisterService(&service);
+    auto server = builder.BuildAndStart();
+
+    // Handshake with CuraEngine
+    boost::asio::co_spawn(
+        grpc_context,
+        [&]() -> boost::asio::awaitable<void>
+        {
+            grpc::ServerContext server_context;
+            grpc::ServerAsyncResponseWriter<proto::PluginResponse> writer{ &server_context };
+            while (true)
+            {
+                proto::PluginRequest request;
+                co_await agrpc::request(&proto::Simplify::AsyncService::RequestIdentify, service, server_context, request, writer, asio::use_awaitable);
+                spdlog::debug("Received an PluginRequest from CuraEngine {}", request.DebugString());
+                proto::PluginResponse response;
+                response.set_slot_id(metadata.slot_id);
+                response.set_plugin_name(metadata.plugin_name);
+                response.set_slot_version(metadata.slot_version);
+                response.set_plugin_version(metadata.plugin_version);
+                co_await agrpc::finish(writer, response, grpc::Status::OK, boost::asio::use_awaitable);
+            }
+        },
+        boost::asio::detached);
+
+    // Start the plugin main process
+    boost::asio::co_spawn(
+        grpc_context,
+        [&]() -> boost::asio::awaitable<void>
+        {
+            grpc::ServerContext server_context;
+            grpc::ServerAsyncResponseWriter<proto::SimplifyResponse> writer{ &server_context };
+            while (true)
+            {
+                proto::SimplifyRequest request;
+                co_await agrpc::request(&proto::Simplify::AsyncService::RequestSimplify, service, server_context, request, writer, boost::asio::use_awaitable);
+                // spdlog::debug("Request: {}", request.DebugString());
+                proto::SimplifyResponse response;
+
+                Simplify simpl(request.max_deviation(), request.max_resolution(), request.max_area_deviation());
+                auto* rsp_polygons = response.mutable_polygons()->add_polygons();
+
+                for (const auto& polygon : request.polygons().polygons())
+                {
+                    const auto& outline = polygon.outline();
+                    geometry::polygon outline_poly{};
+                    for (const auto& point : outline.path())
+                    {
+                        outline_poly.emplace_back(point.x(), point.y());
+                    }
+                    concepts::poly_range auto result_outline = simpl.simplify(outline_poly);
+
+                    auto* rsp_outline = rsp_polygons->mutable_outline();
+                    for (const auto& point : result_outline)
+                    {
+                        auto* rsp_outline_path = rsp_outline->add_path();
+                        rsp_outline_path->set_x(point.X);
+                        rsp_outline_path->set_y(point.Y);
+                    }
+
+                    auto* rsp_holes = rsp_polygons->mutable_holes();
+                    for (const auto& hole : polygon.holes())
+                    {
+                        geometry::polygon holes_poly{};
+                        for (const auto& point : hole.path())
+                        {
+                            holes_poly.emplace_back(point.x(), point.y());
+                        }
+                        concepts::poly_range auto holes_result = simpl.simplify(holes_poly);
+
+                        auto* rsp_hole = rsp_polygons->mutable_holes()->Add();
+                        for (const auto& point : holes_result)
+                        {
+                            auto* hole_path = rsp_hole->add_path();
+                            hole_path->set_x(point.X);
+                            hole_path->set_y(point.Y);
+                        }
+                    }
+                }
+                // spdlog::debug("Response: {}", request.DebugString());
+                co_await agrpc::finish(writer, response, grpc::Status::OK, boost::asio::use_awaitable);
+            }
+        },
+        boost::asio::detached);
+    grpc_context.run();
+
+    server->Shutdown();
 }
